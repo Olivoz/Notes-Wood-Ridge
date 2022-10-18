1224709bc9dabec8560b8b3cf521850331312b82
Olivoz <lucas.sivars@gmail.com>
2022-10-18 09:57:23 +0200

    Display routes

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import AppHeader from "./components/App-Header.vue";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
   <AppHeader></AppHeader>
+  <RouterView />
 </template>
