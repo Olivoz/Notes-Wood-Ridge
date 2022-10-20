c99d2e76240738fdb2c2db008231907f749008d2
Olivoz <lucas.sivars@gmail.com>
2022-10-20 09:50:33 +0200

    Remove redundant onClick prop

--- app/src/components/RoundButton.vue
+++ app/src/components/RoundButton.vue
@@ -1,15 +1,6 @@
-<script>
-export default {
-  props: {
-    onClick: Function,
-  },
-};
-</script>
-
 <template>
   <button
     class="rounded-full fixed right-4 bottom-4 h-12 w-12 p-2 bg-slate-200"
-    @click="onClick"
   >
     <slot />
   </button>
