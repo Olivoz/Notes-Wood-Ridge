41e8012e1a9d951eb63f46d4cc3f9ec3a20120ba
williampettersson <william.c.pettersson@gmail.com>
2022-10-21 17:13:52 +0200

    added username, email and password inputs

--- app/src/views/LoginView.vue
+++ app/src/views/LoginView.vue
@@ -1,8 +1,14 @@
 <script setup></script>
 
 <template>
-  <div>
-    <input type="email" name="email" />
-    <input type="password" name="password" />
+  <div class="rounded-xl text-center justify-center mt-12 py-4 w-full bg-slate-200 dark:bg-zinc-700">
+    <div class="flex justify-center mb-4">
+      <p>Log in to access your notes</p>
+    </div>
+    <input class="text-center mx-2 rounded-xl dark:bg-zinc-600" type="text" name="username" placeholder="Your username"/>
+    <input class="text-center mx-2 mt-4 rounded-xl dark:bg-zinc-600" type="email" name="email" placeholder="Your email" />
+    <br>
+    <input class="text-center mt-8 rounded-xl dark:bg-zinc-600" type="password" name="password" placeholder="Password"/>
   </div>
+  <button class="text-center rounded-xl bg-slate-200 w-32 mt-8 hover:bg-slate-300 dark:bg-zinc-700 dark:text-white">Log in</button>
 </template>
--- app/src/views/RegisterView.vue
+++ app/src/views/RegisterView.vue
@@ -1,5 +1,16 @@
 <script setup></script>
 
 <template>
-  <div></div>
+  <div class="rounded-xl text-center justify-center mt-12 py-4 w-full bg-slate-200 dark:bg-zinc-700">
+    <div class="flex justify-center mb-4">
+      <p class="mx-4">Choose a username:</p>
+      <p class="mx-4">Your email address:</p>
+    </div>
+    <input class="text-center mx-2 rounded-xl dark:bg-zinc-600" type="text" name="username" placeholder="Your username"/>
+    <input class="text-center mx-4 mt-4 rounded-xl dark:bg-zinc-600" type="email" name="email" placeholder="Your email" />
+    <br>
+    <input class="text-center mt-8 rounded-xl dark:bg-zinc-600" type="password" name="password" placeholder="Password"/>
+    <br>
+  </div>
+  <button class="text-center rounded-xl bg-slate-200 w-32 mt-8 hover:bg-slate-300 dark:bg-zinc-700 dark:text-white">Create account</button>
 </template>
