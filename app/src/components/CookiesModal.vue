fd749f86dabdbfb8384399e4b894bf2aabb699cd
williampettersson <william.c.pettersson@gmail.com>
2022-10-24 12:54:54 +0200

    fixed cookies popup

--- app/src/components/CookiesModal.vue
+++ app/src/components/CookiesModal.vue
@@ -1,9 +1,13 @@
-<script setup></script>
+<script setup>
+function close() {
+  document.getElementById("cookieAccept").remove();
+}
+</script>
 
 <template>
-  <div class="container mx-auto px-20 justify-center flex">
+  <div class="container mx-auto px-20 justify-center flex" id="cookieAccept">
     <div
-      class="bg-slate-200 rounded-xl shadow-md p-6 mt-96 text-center"
+      class="absolute bg-slate-200 dark:bg-zinc-800 rounded-xl shadow-md p-6 bottom-4 text-center"
       style="cursor: auto"
     >
       <div class="w-16 mx-auto relative -mt-10 mb-3">
@@ -13,20 +17,22 @@
           alt="Cookie Icon SVG"
         />
       </div>
-      <span class="w-full block leading-normal text-gray-800 text-md mb-3"
+      <span
+        class="w-full block leading-normal text-gray-800 text-md mb-3 dark:text-white"
         >We use cookies to provide a better user experience.</span
       >
       <div class="flex items-center justify-between">
         <button
-          class="text-xs mr-1 text-slate-800 hover:text-gray-500"
+          class="text-xs mr-1 text-slate-800 hover:text-gray-500 dark:text-white dark:hover:text-zinc-400"
           @click="this.$router.push({ path: '/policy' })"
         >
           Privacy Policy
         </button>
         <div class="w-1/2">
           <button
+            @click="close"
             type="button"
-            class="py-2 px-4 bg-white hover:bg-slate-300 w-full text-center text-base font-semibold rounded-lg"
+            class="py-2 px-4 dark:bg-zinc-700 dark:hover:bg-zinc-600 bg-white hover:bg-slate-300 w-full text-center text-base font-semibold rounded-lg"
           >
             Accept
           </button>
--- app/src/views/PrivacyPolicyView.vue
+++ app/src/views/PrivacyPolicyView.vue
@@ -5,10 +5,7 @@ import { Bars3Icon, HomeIcon } from "@heroicons/vue/24/outline";
 <template>
   <div class="text-center mt-24 dark:text-white dark:bg-black">
     <h1 class="semibold text-2xl mt-4">Privacy policy</h1>
-    <p class="semibold text-lg mt-2">
-      We use cookies when users are logging in to provide a better user
-      experience.
-    </p>
+    <p class="semibold text-lg mt-2">We use cookies to keep users logged in.</p>
     <p class="semibold text-lg">Press the <b>home</b> button to return.</p>
     <button
       class="mt-8 rounded-xl bg-slate-200 w-16 hover:bg-slate-300 text-black dark:text-white hover:dark:bg-zinc-500 dark:bg-zinc-700"
