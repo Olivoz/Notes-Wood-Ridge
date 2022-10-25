2abe8d851bf7ced3e2270bb065689000219cb2db
williampettersson <william.c.pettersson@gmail.com>
2022-10-25 13:25:09 +0200

    animated empty recycle bin button

--- app/src/views/RecycleBinView.vue
+++ app/src/views/RecycleBinView.vue
@@ -12,13 +12,15 @@ if (noteStore.trash.length == 0) noteStore.loadTrash();
     <ArrowUturnLeftIcon class="hover:text-green-500 h-6" @click="back" />
   </NoteCard>
   <button
-    class="bg-slate-200 rounded-xl w-full mt-10 py-2 px-6 dark:bg-zinc-800 dark:text-white font-semibold"
+    class="hover:animate-pulse bg-slate-200 rounded-xl w-full mt-10 py-2 px-6 dark:bg-zinc-800 dark:text-white font-semibold"
     v-if="noteStore.trash.length < noteStore.availableTrash"
     @click="noteStore.loadTrash"
   >
     Show more...
   </button>
-  <RoundButton @click="deleteNote" class="bg-red-500 dark:bg-red-500"
+  <RoundButton
+    @click="deleteNote"
+    class="bg-red-500 dark:bg-red-500 hover:animate-bounce"
     ><TrashIcon
   /></RoundButton>
 </template>
