f3e5e9457f60aa5b11a843ec2638deeada7e3d1e
Olivoz <lucas.sivars@gmail.com>
2022-10-26 08:36:45 +0200

    Fix delete and undo buttons in note card

--- app/src/components/NoteCard.vue
+++ app/src/components/NoteCard.vue
@@ -17,15 +17,15 @@ export default {
 </script>
 
 <template>
-  <div @click="editNote" class="mt-10 h-8 dark:text-white">
-    <button
-      class="flex justify-between py-2 px-6 bg-slate-200 rounded-xl w-full dark:bg-zinc-800"
-    >
+  <div
+    class="mt-10 h-8 dark:text-white flex justify-between items-center px-6 bg-slate-200 rounded-xl w-full dark:bg-zinc-800"
+  >
+    <button @click="editNote" class="flex w-full">
       <p class="font-semibold">{{ note.title }}</p>
-      <div class="flex justify-end">
-        <TrashIcon class="hover:text-red-500 h-6" @click="onRemove" />
-        <slot />
-      </div>
     </button>
+    <div class="flex justify-end">
+      <TrashIcon class="hover:text-red-500 h-6" @click="onRemove" />
+      <slot />
+    </div>
   </div>
 </template>
